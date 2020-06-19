335bbedd64af0003292251c7652368a7ca22394c
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-06-19 09:12:11 +0200

    basic staking info

--- src/Staking.js
+++ src/Staking.js
@@ -3,40 +3,38 @@ import { Table, Grid, Button } from 'semantic-ui-react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useSubstrate } from './substrate-lib';
 
-export default function Main (props) {
+export default function Main(props) {
   const { api, keyring } = useSubstrate();
   const accounts = keyring.getPairs();
-  const [balances, setBalances] = useState({});
+  const [validators, setValidators] = useState([]);
 
   useEffect(() => {
-    const addresses = keyring.getPairs().map(account => account.address);
     let unsubscribeAll = null;
 
-    api.query.system.account
-      .multi(addresses, balances => {
-        const balancesMap = addresses.reduce((acc, address, index) => ({
-          ...acc, [address]: balances[index].data.free.toHuman()
-        }), {});
-        setBalances(balancesMap);
-      }).then(unsub => {
-        unsubscribeAll = unsub;
-      }).catch(console.error);
+    api.derive.staking.stashes().then(
+      stashes => stashes.map(api.derive.accounts.info)
+    ).then(promises => {
+      Promise.all(promises).then(setValidators);
+    }).then(unsub => {
+      unsubscribeAll = unsub;
+    }).catch(console.error);
 
     return () => unsubscribeAll && unsubscribeAll();
-  }, [api, keyring, setBalances]);
+  }, [api, setValidators]);
 
   return (
     <Grid.Column>
-      <h1>Balances</h1>
+      <h1>Validators</h1>
+      {JSON.stringify(validators[0])}
       <Table celled striped size='small'>
-        <Table.Body>{accounts.map(account =>
-          <Table.Row key={account.address}>
-            <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
+        <Table.Body>{validators && validators.map(account =>
+          <Table.Row key={account.accountId}>
+            <Table.Cell width={3} textAlign='right'>{account.identity.display}</Table.Cell>
             <Table.Cell width={10}>
               <span style={{ display: 'inline-block', minWidth: '31em' }}>
-                {account.address}
+                {account.accountId.toString()}
               </span>
-              <CopyToClipboard text={account.address}>
+              <CopyToClipboard text={account.accountId}>
                 <Button
                   basic
                   circular
@@ -47,10 +45,7 @@ export default function Main (props) {
                 />
               </CopyToClipboard>
             </Table.Cell>
-            <Table.Cell width={3}>{
-              balances && balances[account.address] &&
-              balances[account.address]
-            }</Table.Cell>
+            <Table.Cell width={3}>{account.identity.email}</Table.Cell>
           </Table.Row>
         )}
         </Table.Body>
